895bf0e035275378db39be57055bdc1803e268e1
VadimFarutin <vfarut@gmail.com>
2018-03-15 18:58:18 +0300

    Checking whether subdirectory exists or not

--- subdirectories_build.sh
+++ subdirectories_build.sh
@@ -7,15 +7,18 @@ build_subdirectory () {
   do
     #printf "%s\n" "$name"
 
-    cd $name
-
-    if [ -f "gradlew" ]
+    if [ -d "$name"]
     then
-      chmod +x gradlew
-      ./gradlew build
-    fi
+      cd $name
 
-    cd ..
+      if [ -f "gradlew" ]
+      then
+        chmod +x gradlew
+        ./gradlew build
+      fi
+
+      cd ../..
+    fi
   done
 
   exit
